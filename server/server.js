c1c32e470811a225286bd8badd4c633a43c2a1a6
RAKIB <rakibmahmudmridha@gmail.com>
2024-05-08 19:40:19 +0600

    mongodb set

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+connectDB();
 const app = express();
 
 app.listen(4000, () => {

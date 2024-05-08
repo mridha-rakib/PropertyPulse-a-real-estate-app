1cb3f1367cdea052a1042502302a972db4515761
RAKIB <rakibmahmudmridha@gmail.com>
2024-05-08 20:27:48 +0600

    signin backend

--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ import connectDB from "./config/db.js";
 connectDB();
 const app = express();
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(4000, () => {
   console.log(`Server is listening on port 4000`);
 });

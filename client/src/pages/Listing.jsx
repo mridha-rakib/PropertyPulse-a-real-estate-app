1bdc0d29a3aec2d76406791fab1961f09f1dc212
RAKIB <rakibmahmudmridha@gmail.com>
2024-05-21 15:06:51 +0600

    image slider complete

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,67 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useSelector } from "react-redux";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
 const Listing = () => {
-  return <div>Listing</div>;
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        console.log(data);
+        if (data.success === "false") {
+          console.log(data);
+          setError(true);
+          setLoading(false);
+          return;
+        }
+
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [params.listingId]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="h-[550px] w-full  "
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "contain",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Listing;
